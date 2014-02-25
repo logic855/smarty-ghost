06f2e58fc21510c2c4eff3dccad21b2b885db229
Christopher Horrell <christopher@horrell.ca>
2014-02-24 22:08:40 -0500

    Provide info on progress and prompt user for url. Also removed setting hostname

--- install-ghost.sh
+++ install-ghost.sh
@@ -3,7 +3,11 @@
 ## Download and install Ghost (https://ghost.org) on a Joyent SmartOS instance.
 
 set -o errexit
-set -o xtrace
+
+# For debugging
+if [ $DEBUG -eq 1 ]; then
+  set -o xtrace
+fi
 
 # Set common vars. Change these as required.
 pkgpath=/opt/local
@@ -11,28 +15,41 @@ ghosthome=/home/admin
 ghostversion=0.4.1
 WD=$(pwd)
 publicIP=$(mdata-get sdc:nics.0.ip)
-HOSTNAME=example.com
-URL=http://${HOSTNAME}
+
+echo "Preparing to install Ghost $ghostversion."
+echo ""
+echo ""
+echo "Enter the URL of your Ghost blog (e.g., http://example.com), followed by [ENTER]:"
+read url
 
 # Ensure the latest version of Node.js is installed, as well as other required
 # packages.
-pkgin -y update
-pkgin -y full-upgrade
-pkgin -y install nodejs curl unzip gmake gcc47
+echo -n "Updating packages..."
+pkgin -y update > /dev/null
+pkgin -y full-upgrade > /dev/null
+echo "done."
 
-# Set hostname
-sm-set-hostname ${HOSTNAME}
+echo -n "Installing latest version of Node.js and other required packages..."
+pkgin -y install nodejs curl unzip gmake gcc47 > /dev/null
+echo "done."
 
-# Dowload and unzip ghost
-${pkgpath}/bin/curl -o ${ghosthome}/ghost.zip -L \
-  https://ghost.org/zip/ghost-${ghostversion}.zip
+# Dowload and unzip Ghost
+echo -n "Downloading ghost-${ghostversion}.zip and saving as ${ghosthome}/ghost.zip..."
+${pkgpath}/bin/curl -s -o ${ghosthome}/ghost.zip -L \
+  https://ghost.org/zip/ghost-${ghostversion}.zip > /dev/null
+echo "done."
 
-${pkgpath}/bin/unzip ${ghosthome}/ghost.zip -d ${ghosthome}/ghost
+echo -n "Unzipping ${ghosthome}/ghost.zip..."
+${pkgpath}/bin/unzip ${ghosthome}/ghost.zip -d ${ghosthome}/ghost > /dev/null
+echo "done."
 
+echo -n "Installing Ghost via npm..."
 cd ${ghosthome}/ghost
-npm install --production
+npm install --production --silent
 cd $HOME
+echo "done."
 
+echo -n "Creating Ghost config.js in ${ghosthome}/ghost/..."
 # Create Ghost config
 cat << ghostconfig > ${ghosthome}/ghost/config.js
 var path = require('path'),
@@ -58,10 +75,13 @@ config = {
 
 module.exports = config;
 ghostconfig
+echo "done."
 
+# Ensure the right ownership is setup for the SMF service
 chown admin:staff -R ${ghosthome}/ghost/
 
 # Create SMF manifest for Ghost service
+echo -n "Creating SMF service..."
 cat << SMF > ${ghosthome}/ghost/ghost.xml
 <?xml version="1.0"?>
 <!DOCTYPE service_bundle SYSTEM '/usr/share/lib/xml/dtd/service_bundle.dtd.1'>
@@ -108,9 +128,10 @@ chown admin:staff ${ghosthome}/ghost/ghost.xml
 # Install and enble the SMF service for Ghost
 svccfg import ${ghosthome}/ghost/ghost.xml
 svcadm enable ghost
+echo "done."
 
 echo ""
-echo "Ghost is now installed and available at http://$publicIP/"
+echo "Ghost is now installed and available at http://$publicIP/ (or $URL if the correct DNS records have been setup)."
 echo ""
 
 exit 0
